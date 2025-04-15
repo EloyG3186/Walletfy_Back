070876bd858bbf69eefbeba3f50900e66a916036
EloyG3186 <eloy.gonzalezja2@gmail.com>
2025-04-14 21:04:11 -0500

    Resuelvo conflicto

--- src/middlewares/utf8Encoding.js
+++ src/middlewares/utf8Encoding.js
@@ -1,25 +1,14 @@
 /**
-<<<<<<< HEAD
  * Middleware para asegurar la codificación UTF-8 en todas las respuestas
-=======
- * Middleware para asegurar la codificacion UTF-8 en todas las respuestas
->>>>>>> 6cafcb5a073de74189b94d0efffd0f96a288afd6
  */
 const utf8Encoding = (req, res, next) => {
   // Configurar encabezados para UTF-8
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   
-<<<<<<< HEAD
   // Preservar el método json original
   const originalJson = res.json;
   
   // Sobrescribir el método json para asegurar que los datos se codifiquen correctamente
-=======
-  // Preservar el metodo json original
-  const originalJson = res.json;
-  
-  // Sobrescribir el metodo json para asegurar que los datos se codifiquen correctamente
->>>>>>> 6cafcb5a073de74189b94d0efffd0f96a288afd6
   res.json = function(obj) {
     return originalJson.call(this, obj);
   };
